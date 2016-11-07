f5dfc54c4d23b53d0bbc67a1cd2d6a13eaf86731
2020m <sylvain.marleau@videotron.ca>
2016-11-07 08:38:13 -0500

    Create wifi.sh

---
+++ wifi.sh
@@ -0,0 +1,5 @@
+sudo pacman -S --noconfirm dialog
+sudo pacman -S --noconfirm wpa_supplicant
+# to configure the wifi network disconnect the network wire and install the wifi antenna
+# sudo wifi-menu
+# sudo netctl enable wlan0-FM-BldgSys-R24-1
