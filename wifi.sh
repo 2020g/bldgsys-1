8ec220e9e40a0a9ce8866a24ac2bb85f197bb63d
2020m <sylvain.marleau@videotron.ca>
2016-11-07 08:40:49 -0500

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,5 +1,5 @@
 sudo pacman -S --noconfirm dialog
 sudo pacman -S --noconfirm wpa_supplicant
 # to configure the wifi network disconnect the network wire and install the wifi antenna
-# sudo wifi-menu
-# sudo netctl enable wlan0-FM-BldgSys-R24-1
+sudo wifi-menu
+sudo netctl enable wlan0-FM-BldgSys-R24-1
