588387606bd5fa8da0675bc9fa1bc8d69b919126
2020m <sylvain.marleau@videotron.ca>
2016-11-07 07:55:40 -0500

    Create 1-al-c2-ssd.sh

---
+++ ARMv8/1-al-c2-ssd.sh
@@ -0,0 +1,35 @@
+# see https://archlinuxarm.org/platforms/armv8/amlogic/odroid-c2
+echo "Micro SD and eMMC Creation on Odroid-C2"
+echo "Run this script as root only."
+echo "You can hit Ctrl+c in next 10 seconds if you don't want to continue."
+sleep 10
+
+umount /dev/sdd1
+
+dd if=/dev/zero of=/dev/sdd bs=1M count=8
+
+fdisk /dev/sdd << __EOF__ >> /dev/null
+o
+p
+n
+p
+1
+
+
+w
+__EOF__
+
+mkfs.ext4 /dev/sdd1
+
+mkdir root
+mount /dev/sdd1 root
+
+wget http://archlinuxarm.org/os/ArchLinuxARM-odroid-c2-latest.tar.gz
+bsdtar -xpf ArchLinuxARM-odroid-c2-latest.tar.gz -C root
+
+cd root/boot
+./sd_fusing.sh /dev/sdd
+cd ../..
+
+umount root
+echo "Creation completed"
