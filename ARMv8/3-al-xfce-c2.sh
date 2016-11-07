8feb472dd74f5ab21db208f08a6b6c4c21759652
2020m <sylvain.marleau@videotron.ca>
2016-11-07 08:13:17 -0500

    Create 3-al-xfce-c2.sh

---
+++ ARMv8/3-al-xfce-c2.sh
@@ -0,0 +1,19 @@
+# Post installation
+echo "Run this script as ALARM only."
+echo "Post installation."
+echo "Install & configure Archlinux/XFCE on Odroid-C2"
+echo "You can hit Ctrl+c in next 5 seconds if you don't want to continue."
+sleep 5
+sudo localectl set-locale LANG=en_US.UTF-8
+sudo timedatectl set-timezone America/Montreal
+sudo localedef -f UTF-8 -i en_US en_US.UTF-8
+#sudo pluma /etc/locale.gen add en_US en_US.UTF-8 and sudo locale-gen
+sudo hostnamectl set-hostname FM-BldgSys-E0000
+sudo pacman -S --noconfirm xorg-xinit 
+sudo pacman -S --noconfirm xorg-server 
+sudo pacman -S --noconfirm xf86-video-odroid-c2
+sudo pacman -S --noconfirm xfce4
+sudo pacman -S --noconfirm lightdm
+sudo pacman -S --noconfirm lightdm-gtk-greeter
+sudo systemctl enable lightdm.service
+sudo systemctl start lightdm.service
