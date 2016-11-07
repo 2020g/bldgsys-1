cf0a66888b018f04da26b9430634373b73036b64
2020m <sylvain.marleau@videotron.ca>
2016-11-07 08:34:27 -0500

    Update 3-al-xfce.sh

--- 3-al-xfce.sh
+++ 3-al-xfce.sh
@@ -7,12 +7,16 @@ sleep 5
 sudo localectl set-locale LANG=en_US.UTF-8
 sudo timedatectl set-timezone America/Montreal
 sudo localedef -f UTF-8 -i en_US en_US.UTF-8
-#sudo pluma /etc/locale.gen add en_US en_US.UTF-8 and sudo locale-gen
+cd /etc
+sudo wget -N https://raw.githubusercontent.com/2020m/bldgsys/master/locale.gen
+sudo locale-gen
 sudo hostnamectl set-hostname FM-BldgSys-E0000
 sudo pacman -S --noconfirm xorg-xinit 
 sudo pacman -S --noconfirm xorg-server 
 sudo pacman -S --noconfirm xfce4
 sudo pacman -S --noconfirm lightdm
 sudo pacman -S --noconfirm lightdm-gtk-greeter
+cd /etc/lightdm
+sudo wget -N https://raw.githubusercontent.com/2020m/bldgsys/master/lightdm.conf
 sudo systemctl enable lightdm.service
 sudo systemctl start lightdm.service
